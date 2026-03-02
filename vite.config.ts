b950a7039205d2dc87f90bc8524eaadc87cf7f5c
Mateo Garetto <TU_EMAIL_DE_GITHUB>
2026-03-01 22:02:11 -0300

    fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/HOPE-CONTENEDORES/', // <--- ESTA ES LA LÍNEA QUE AGREGUÉ
+    base: '/', // <--- ESTA ES LA LÍNEA QUE AGREGUÉ
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
